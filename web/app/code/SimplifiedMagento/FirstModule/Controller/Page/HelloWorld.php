b24e18c5ac5a1d7fda46c32a8c7d9a38907693e0
Alexandr Martynov <alex@magemore.com>
2021-06-19 22:58:47 +0300

    Hello worl

---
+++ web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SimplifiedMagento\FirstModule\Controller\Page;
+
+class HelloWorld extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo "Hello World";
+    }
+}
