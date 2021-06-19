cb70dd5996c5538c0f572e518ee891d7dd427914
Alexandr Martynov <alex@magemore.com>
2021-06-19 23:26:36 +0300

    ProductRepositoryInterface

--- web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -3,6 +3,7 @@
 namespace SimplifiedMagento\FirstModule\Controller\Page;
 use Magento\Framework\App\Action\Context;
 use SimplifiedMagento\FirstModule\NotMagento\PencilInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
