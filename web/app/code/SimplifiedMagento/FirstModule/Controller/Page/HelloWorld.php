a0520fe92a61bda919777e7447fe35bcb0004eb4
Alexandr Martynov <alex@magemore.com>
2021-06-19 23:13:28 +0300

    inject PencilInterface

--- web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -1,9 +1,18 @@
 <?php
 
 namespace SimplifiedMagento\FirstModule\Controller\Page;
+use Magento\Framework\App\Action\Context;
+use SimplifiedMagento\FirstModule\NotMagento\PencilInterface;
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
+
+
+    public function __construct(Context $context, PencilInterface $pencil)
+    {
+        parent::__construct($context);
+    }
+
     public function execute()
     {
         echo "Hello World";
