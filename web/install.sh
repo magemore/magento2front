dabf2147d1a1f321cc757a0fdfc6f4843176d39f
Alexandr Martynov <alex@magemore.com>
2021-06-19 22:40:24 +0300

    fix

---
+++ web/install.sh
@@ -0,0 +1,15 @@
+bin/magento setup:install \
+--base-url=http://magento2front.loc \
+--db-host=localhost \
+--db-name=magento2front \
+--db-user=root \
+--db-password=q1234e \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=admin@admin.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=USD \
+--timezone=America/Chicago \
+--use-rewrites=1
