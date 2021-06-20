f853538eec811cd35f96a188e55722e74f59a5af
Alexandr Martynov <alex@magemore.com>
2021-06-21 01:20:13 +0300

    cli

--- web/app/code/SimplifiedMagento/FirstModule/Console/Command/HelloWorld.php
+++ web/app/code/SimplifiedMagento/FirstModule/Console/Command/HelloWorld.php
@@ -5,6 +5,7 @@ namespace SimplifiedMagento\FirstModule\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class HelloWorld extends Command
 {
@@ -20,6 +21,6 @@ class HelloWorld extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello World');
+        $output->writeln('Hello World, '.$input->getArgument('name'));
     }
 }
