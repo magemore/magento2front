efbae15796cadf2fd7d5d7f03875ca428dfeaec9
Alexandr Martynov <alex@magemore.com>
2021-06-20 12:48:10 +0300

    getPencilType

---
+++ web/app/code/SimplifiedMagento/FirstModule/Model/Pencil.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace SimplifiedMagento\FirstModule\Model;
+use SimplifiedMagento\FirstModule\Api\PencilInterface;
+use SimplifiedMagento\FirstModule\Api\Color;
+use SimplifiedMagento\FirstModule\Api\Size;
+
+class Pencil implements PencilInterface
+{
+
+    protected $color;
+    protected $size;
+
+    public function __constructor(Color $color, Size $size)
+    {
+        $this->color = $color;
+        $this->size = $size;
+    }
+
+    public function getPencilType()
+    {
+        return "pencil has ".$this->color." Color and ".$this->size." Size";
+    }
+}
