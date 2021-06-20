49f4c3373d1da03c89860a9b4fd522f2c58f5b55
Alexandr Martynov <alex@magemore.com>
2021-06-20 12:32:37 +0300

    SimplifiedMagento\FirstModule\Model\CustomImplementation\Interceptor

--- web/app/code/SimplifiedMagento/FirstModule/Model/CustomImplementation.php
+++ web/app/code/SimplifiedMagento/FirstModule/Model/CustomImplementation.php
@@ -4,36 +4,56 @@
 namespace SimplifiedMagento\FirstModule\Model;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
 
 class CustomImplementation implements ProductRepositoryInterface
 {
 
-    public function save(Data\ProductInterface $product, $saveOptions = false)
+    /**
+     * @inheritDoc
+     */
+    public function save(ProductInterface $product, $saveOptions = false)
     {
         // TODO: Implement save() method.
     }
 
+    /**
+     * @inheritDoc
+     */
     public function get($sku, $editMode = false, $storeId = null, $forceReload = false)
     {
         // TODO: Implement get() method.
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getById($productId, $editMode = false, $storeId = null, $forceReload = false)
     {
         // TODO: Implement getById() method.
     }
 
-    public function delete(Data\ProductInterface $product)
+    /**
+     * @inheritDoc
+     */
+    public function delete(ProductInterface $product)
     {
         // TODO: Implement delete() method.
     }
 
+    /**
+     * @inheritDoc
+     */
     public function deleteById($sku)
     {
         // TODO: Implement deleteById() method.
     }
 
-    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    /**
+     * @inheritDoc
+     */
+    public function getList(SearchCriteriaInterface $searchCriteria)
     {
         // TODO: Implement getList() method.
     }
