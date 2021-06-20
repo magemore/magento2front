dda4e5f3044dc8df3e7d0a499d08745b10c81896
Alexandr Martynov <alex@magemore.com>
2021-06-20 15:54:00 +0300

    scores

--- web/app/code/SimplifiedMagento/FirstModule/Model/Student.php
+++ web/app/code/SimplifiedMagento/FirstModule/Model/Student.php
@@ -8,8 +8,10 @@ class Student
 {
     private $name;
     private $age;
-    public function __construct($name = "Alex", $age = 28) {
+    private $scores;
+    public function __construct($name = "Alex", $age = 28, array $scores = array('maths'=>92, 'programming'=>90)) {
         $this->name = $name;
         $this->age = $age;
+        $this->scores = $scores;
     }
 }
