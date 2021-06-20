92f987e249ee4ea9f739a57bc9f6b180768b87eb
Alexandr Martynov <alex@magemore.com>
2021-06-20 20:58:11 +0300

    around plugin

--- web/app/code/SimplifiedMagento/FirstModule/Plugin/PluginSolution.php
+++ web/app/code/SimplifiedMagento/FirstModule/Plugin/PluginSolution.php
@@ -6,12 +6,18 @@ namespace SimplifiedMagento\FirstModule\Plugin;
 
 class PluginSolution
 {
-    public function beforeSetName(\Magento\Catalog\Model\Product $subject, $name)
-    {
-        return "Before Plugin " . $name;
-    }
+//    public function beforeSetName(\Magento\Catalog\Model\Product $subject, $name)
+//    {
+//        return "Before Plugin " . $name;
+//    }
 
 //    public function afterGetName(\Magento\Catalog\Model\Product $subject, $result) {
 //        return $result . " After Plugin";
 //    }
+
+    public function aroundGetName(\Magento\Catalog\Model\Product $subject, callable $proceed)
+    {
+        echo "before proceed";
+        $name = proceed();
+    }
 }
