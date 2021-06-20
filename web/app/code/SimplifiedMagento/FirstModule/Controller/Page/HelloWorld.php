2e4fc43efe4e61d5a9e428931619b6c4805897e3
Alexandr Martynov <alex@magemore.com>
2021-06-20 16:52:48 +0300

    pencilFactory create

--- web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ web/app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -4,29 +4,31 @@ namespace SimplifiedMagento\FirstModule\Controller\Page;
 use Magento\Framework\App\Action\Context;
 use SimplifiedMagento\FirstModule\Api\PencilInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use SimplifiedMagento\FirstModule\Model\PencilFactory;
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
 
     protected $pencilInteface;
     protected $productRepository;
+    protected $pencilFactory;
 
-    public function __construct(Context $context, PencilInterface $pencilInteface, ProductRepositoryInterface $productRepository)
+    public function __construct(Context $context,
+                                PencilFactory $pencilFactory,
+                                PencilInterface $pencilInteface, ProductRepositoryInterface $productRepository)
     {
         $this->pencilInteface = $pencilInteface;
         $this->productRepository = $productRepository;
+        $this->pencilFactory = $pencilFactory;
         parent::__construct($context);
     }
 
     public function execute()
     {
-//        echo $this->pencilInteface->getPencilType();
-//        echo get_class($this->productRepository);
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $book = $objectManager->create('SimplifiedMagento\FirstModule\Model\Book');
-        echo '<pre>';
-        var_dump($book);
-        echo '</pre>';
+        $pencil = $this->pencilFactory->create(array("name"=>"Alex"));
+        echo "<pre>";
+        var_dump($pencil);
+        echo "</pre>";
 
     }
 
