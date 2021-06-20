1392435ef341ef25fb54a0e5604d701b63447869
Alexandr Martynov <alex@magemore.com>
2021-06-20 16:48:00 +0300

    PencilInterface PencilFactory

--- web/app/code/SimplifiedMagento/FirstModule/Model/PencilFactory.php
+++ web/app/code/SimplifiedMagento/FirstModule/Model/PencilFactory.php
@@ -10,4 +10,8 @@ class PencilFactory
     public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager) {
         $this->objectManager = $objectManager;
     }
+
+    public function create(array $data) {
+        return $this->objectManager->create('SimplifiedMagento\FirstModule\Api\PencilInterface');
+    }
 }
