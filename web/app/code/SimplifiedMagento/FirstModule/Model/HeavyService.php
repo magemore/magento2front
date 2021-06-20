7eab67728ddd83531338ce74585c4debd67a1e82
Alexandr Martynov <alex@magemore.com>
2021-06-21 00:07:21 +0300

    Heavy Service Proxy

---
+++ web/app/code/SimplifiedMagento/FirstModule/Model/HeavyService.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace SimplifiedMagento\FirstModule\Model;
+
+
+class HeavyService
+{
+    public function __construct()
+    {
+        echo "HeavyService has been installated";
+    }
+}
