2f6a44ce8be5a6349a613142c0fef284cf978781
Alexandr Martynov <alex@magemore.com>
2021-06-20 12:10:49 +0300

    CustomImplementation ProductRepositoryInterface preference di.xml

--- web/app/code/SimplifiedMagento/FirstModule/Model/CustomImplementation.php
+++ web/app/code/SimplifiedMagento/FirstModule/Model/CustomImplementation.php
@@ -3,9 +3,40 @@
 
 namespace SimplifiedMagento\FirstModule\Model;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
 yse Magento\Catalog\Api\ProductRepositoryInterface;
 
-class CustomImplementation
+class CustomImplementation implements ProductRepositoryInterface
 {
 
+    public function save(Data\ProductInterface $product, $saveOptions = false)
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function get($sku, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function getById($productId, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function delete(Data\ProductInterface $product)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function deleteById($sku)
+    {
+        // TODO: Implement deleteById() method.
+    }
+
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        // TODO: Implement getList() method.
+    }
 }
