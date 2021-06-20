7d14a7e9b68a48e487a8dd8776c52fcff224a9e3
Alexandr Martynov <alex@magemore.com>
2021-06-21 01:07:01 +0300

    cli

---
+++ web/app/code/SimplifiedMagento/FirstModule/Console/Command/HelloWorl.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace SimplifiedMagento\FirstModule\Console\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloWorl extends Command
+{
+    public function configure()
+    {
+        $this->setName('training:hello_world')
+            ->setDescription('the command prints out hello world')
+            ->setAliases(array('hw'));
+    }
+
+    public function execute()
+    {
+
+    }
+}
