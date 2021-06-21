5886339612ee8d8d112660f971381c0b482c9fb6
Alexandr Martynov <alex@magemore.com>
2021-06-21 16:20:40 +0300

    Router

---
+++ web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace SimplifiedMagento\RequestFlow\Controller\Page;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\RouterInterface;
+
+class Router implements RouterInterface
+{
+
+    public function match(RequestInterface $request)
+    {
+        // TODO: Implement match() method.
+    }
+}
