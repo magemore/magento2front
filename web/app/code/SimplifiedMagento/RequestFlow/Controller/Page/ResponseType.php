23d49a59331028d6ba2b7a023f85391122780fc0
Alexandr Martynov <alex@magemore.com>
2021-06-21 19:56:51 +0300

    rawFactory

--- web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
+++ web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
@@ -8,25 +8,29 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\Result\JsonFactory;
-use Magento\Framework\Controller\Result\RowFactory;
+use Magento\Framework\Controller\Result\RawFactory;
 
 class ResponseType extends Action
 {
 
     protected $pageFactory;
     protected $jsonFactory;
-    protected $rowFactory;
+    protected $rawFactory;
 
-    public function __construct(Context $context, PageFactory $pageFactory, JsonFactory $jsonFactory, RowFactory $rowFactory)
+    public function __construct(Context $context, PageFactory $pageFactory, JsonFactory $jsonFactory, RawFactory $rawFactory)
     {
         $this->pageFactory = $pageFactory;
         $this->jsonFactory = $jsonFactory;
+        $this->rawFactory = $rawFactory;
         parent::__construct($context);
     }
 
     public function execute()
     {
 //        return $this->pageFactory->create();
-        return $this->jsonFactory->create()->setData(['key'=>'value','key2'=>['one','two']]);
+//        return $this->jsonFactory->create()->setData(['key'=>'value','key2'=>['one','two']]);
+        $result = $this->rawFactory->setContents('Hello World');
+        return $result;
     }
+
 }
