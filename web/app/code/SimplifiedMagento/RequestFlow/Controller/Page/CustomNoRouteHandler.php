3aa93bbb75f37bc29ea137128a86b44c28480965
Alexandr Martynov <alex@magemore.com>
2021-06-21 14:29:58 +0300

    NoRouteHandlerInterface

---
+++ web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/CustomNoRouteHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace SimplifiedMagento\RequestFlow\Controller\Page;
+
+use \Magento\Framework\App\Router\NoRouteHandlerInterface;
+
+class CustomNoRouteHandler implements NoRouteHandlerInterface
+{
+
+    public function process(\Magento\Framework\App\RequestInterface $request)
+    {
+        // TODO: Implement process() method.
+    }
+}
