b8e3b3c87e57857a05e405c662af2dd677b4d823
Alexandr Martynov <alex@magemore.com>
2021-06-21 21:57:24 +0300

    database install schema setup

--- web/app/code/SimplifiedMagento/Database/Setup/InstallSchema.php
+++ web/app/code/SimplifiedMagento/Database/Setup/InstallSchema.php
@@ -2,6 +2,20 @@
 
 namespace SimplifiedMagento\Database\Setup;
 
-class InstallSchema {
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
 
+class InstallSchema implements InstallSchemaInterface {
+
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->endSetup();
+    }
 }
