d2f1a756b4f298d66c0f73ccc316b9bbc22cd003
Alexandr Martynov <alex@magemore.com>
2021-06-21 16:28:23 +0300

    action factory forward

--- web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/Router.php
+++ web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/Router.php
@@ -5,13 +5,27 @@ namespace SimplifiedMagento\RequestFlow\Controller\Page;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\RouterInterface;
+use Magento\Framework\App\ActionFactory;
 
 class Router implements RouterInterface
 {
+    protected $actionFactory;
+
+    public function __construct(ActionFactory $actionFactory)
+    {
+        $this->actionFactory = $actionFactory;
+    }
 
     public function match(RequestInterface $request)
     {
         // /customer-account-login
         $path = $request->getPathInfo();
+        $path = trim($path,'/');
+        $paths = explode('-',$path);
+        $request->setModuleName($paths[0]);
+        $request->setControllerName($paths[1]);
+        $request->setActionName($paths[2]);
+
+        return $this->actionFactory->create('Magento\Framework\App\Action\Forward', ['request']);
     }
 }
