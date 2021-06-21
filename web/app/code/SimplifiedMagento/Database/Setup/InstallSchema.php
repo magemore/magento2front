4b8c56be89a65e36213efd999f65f81de6fce49b
Alexandr Martynov <alex@magemore.com>
2021-06-21 22:22:55 +0300

    newTable fix

--- web/app/code/SimplifiedMagento/Database/Setup/InstallSchema.php
+++ web/app/code/SimplifiedMagento/Database/Setup/InstallSchema.php
@@ -16,46 +16,44 @@ class InstallSchema implements InstallSchemaInterface {
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-        $table = $setup->getConnection()->newTable(
-            $setup->getTable('affiliate_member')
-                ->addColumn(
-                    'entity_id',
-                    Table::TYPE_INTEGER,
-                    null,
-                    ['identity' =>true, 'nullable'=>false, 'primary' =>true],
-                    'MEMBER ID'
-                )->addColumn(
-                    'name',
-                    Table::TYPE_TEXT,
-                    255,
-                    ['nullable' => false],
-                    'NAME OF MEMBER'
-                )->addColumn(
-                    'address',
-                    Table::TYPE_TEXT,
-                    255,
-                    ['nullable' => false],
-                    'ADDRESS OF MEMBER'
-                )->addColumn(
-                    'status',
-                    Table::TYPE_BOOLEAN,
-                    10,
-                    ['nullable' => false, 'default' => false],
-                    'STATUS'
-                )->addColumn(
-                    'created_at',
-                    Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default'=>Table::TIMESTAMP_INIT],
-                    'TIME CREATED'
-                )->addColumn(
-                    'updated_at',
-                    Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default'=>Table::TIMESTAMP_INIT],
-                    'TIME UPDATED'
-                )->setComment('Affiliate Member Table')
-        );
+        $table = $setup->getConnection()->newTable($setup->getTable('affiliate_member'))
+            ->addColumn(
+                'entity_id',
+                Table::TYPE_INTEGER,
+                null,
+                ['identity' =>true, 'nullable'=>false, 'primary' =>true],
+                'MEMBER ID'
+            )->addColumn(
+                'name',
+                Table::TYPE_TEXT,
+                255,
+                ['nullable' => false],
+                'NAME OF MEMBER'
+            )->addColumn(
+                'address',
+                Table::TYPE_TEXT,
+                255,
+                ['nullable' => false],
+                'ADDRESS OF MEMBER'
+            )->addColumn(
+                'status',
+                Table::TYPE_BOOLEAN,
+                10,
+                ['nullable' => false, 'default' => false],
+                'STATUS'
+            )->addColumn(
+                'created_at',
+                Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default'=>Table::TIMESTAMP_INIT],
+                'TIME CREATED'
+            )->addColumn(
+                'updated_at',
+                Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default'=>Table::TIMESTAMP_INIT],
+                'TIME UPDATED'
+            )->setComment('Affiliate Member Table');
         $setup->getConnection()->createTable($table);
         $setup->endSetup();
     }
