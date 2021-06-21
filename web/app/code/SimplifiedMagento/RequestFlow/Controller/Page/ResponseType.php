66e43f78ae71ee3fe92c8a6642bd2115e4c9d358
Alexandr Martynov <alex@magemore.com>
2021-06-21 16:39:50 +0300

    ResponseType

--- web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
+++ web/app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
@@ -3,8 +3,23 @@
 
 namespace SimplifiedMagento\RequestFlow\Controller\Page;
 
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
 
-class ResponseType
+class ResponseType extends Action
 {
 
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
 }
