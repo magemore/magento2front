53c1b178c6e6850d18a756751c560ed9b181776d
Alexandr Martynov <alex@magemore.com>
2021-06-22 13:36:48 +0300

    collection

--- web/app/code/SimplifiedMagento/Database/Model/Resource/AffiliateMember/Collection.php
+++ web/app/code/SimplifiedMagento/Database/Model/Resource/AffiliateMember/Collection.php
@@ -4,8 +4,14 @@
 namespace SimplifiedMagento\Database\Model\Resource\AffiliateMember;
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use SimplifiedMagento\Database\Model\AffiliateMember;
+use SimplifiedMagento\Database\Model\Resource\AffiliateMember as AffiliateMemberResource;
 
 class Collection extends AbstractCollection
 {
-
+    protected function __construct()
+    {
+        parent::__construct();
+        $this->_init();
+    }
 }
