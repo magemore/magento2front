3b0a1f88a6498b45b9bc586b3a7d00fcd4c19ec3
Alexandr Martynov <alex@magemore.com>
2021-06-22 04:10:09 +0300

    setup data

---
+++ web/app/code/SimplifiedMagento/Database/Setup/InstallData.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace SimplifiedMagento\Database\Setup;
+
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->getConnection()->insert(
+            $setup->getTable('affiliate_member'),
+            ['name'=>'Bob', 'address' => 'No 10, Dubai', 'status'=>true]
+        );
+        $setup->getConnection()->insert(
+            $setup->getTable('affiliate_member'),
+            ['name'=>'Alex', 'address' => 'No 11, Dubai']
+        );
+
+        $setup->endSetup();
+    }
+}
