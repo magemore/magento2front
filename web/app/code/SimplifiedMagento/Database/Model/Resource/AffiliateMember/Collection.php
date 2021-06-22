aa28289253e134d94857ab71b938af9081f30d06
Alexandr Martynov <alex@magemore.com>
2021-06-22 13:34:52 +0300

    collection

---
+++ web/app/code/SimplifiedMagento/Database/Model/Resource/AffiliateMember/Collection.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace SimplifiedMagento\Database\Model\Resource\AffiliateMember;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+
+}
