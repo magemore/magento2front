db73c8e3374b64a8702efde6016732d757d6a285
Alexandr Martynov <alex@magemore.com>
2021-06-22 04:35:05 +0300

    AffiliateMember model

--- web/app/code/SimplifiedMagento/Database/Model/AffiliateMember.php
+++ web/app/code/SimplifiedMagento/Database/Model/AffiliateMember.php
@@ -4,8 +4,12 @@
 namespace SimplifiedMagento\Database\Model;
 
 use Magento\Framework\Model\AbstractModel;
+use SimplifiedMagento\Database\Model\Resource\AffiliateMember;
 
 class AffiliateMember extends AbstractModel
 {
-
+    protected function _construct()
+    {
+        $this->_init(AffiliateMember::class);
+    }
 }
