e614ad0dc7027901baf0831b0fd1338354334a01
Alexandr Martynov <alex@magemore.com>
2021-06-22 04:20:13 +0300

    upgrade data

--- web/app/code/SimplifiedMagento/Database/Setup/UpgradeData.php
+++ web/app/code/SimplifiedMagento/Database/Setup/UpgradeData.php
@@ -4,7 +4,23 @@
 namespace SimplifiedMagento\Database\Setup;
 
 
-class UpgradeData
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeData implements UpgradeSchemaInterface
 {
 
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+
+
+        $setup->endSetup();
+    }
 }
