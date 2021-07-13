6636886d3be141f54f18b940f4ba2b107d84445e
Alexandr Martynov <alex@magemore.com>
2021-07-13 17:26:58 +0300

    api

---
+++ web/app/code/SimplifiedMagento/Database/Api/AffiliateMemberRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace SimplifiedMagento\Database\Api;
+
+// continue https://www.udemy.com/course/simplified-magento-2-video-course-from-beginner-to-expert/learn/lecture/13787714#overview
+// 19:15
+interface AffiliateMemberRepositoryInterface
+{
+
+}
