a904bc97e32b7916d6fc79fd6c723ef43ebd5a17
Alexandr Martynov <alex@magemore.com>
2021-07-13 17:30:05 +0300

    AffiliateMemberRepositoryInterface getList

--- web/app/code/SimplifiedMagento/Database/Api/AffiliateMemberRepositoryInterface.php
+++ web/app/code/SimplifiedMagento/Database/Api/AffiliateMemberRepositoryInterface.php
@@ -3,9 +3,12 @@
 
 namespace SimplifiedMagento\Database\Api;
 
-// continue https://www.udemy.com/course/simplified-magento-2-video-course-from-beginner-to-expert/learn/lecture/13787714#overview
-// 19:15
+
 interface AffiliateMemberRepositoryInterface
 {
+    /*
+     * @return \SimplifiedMagento\Database\Api\Data\AffiliateMemberInterface[]
+     */
+    public function getList();
 
 }
