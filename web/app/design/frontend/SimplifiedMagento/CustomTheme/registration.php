29b8dc288cf38a7eb8e69a8aa11f6c096d0425c2
Alexandr Martynov <alex@magemore.com>
2021-07-15 22:57:54 +0300

    custom theme registration

--- web/app/design/frontend/SimplifiedMagento/CustomTheme/registration.php
+++ web/app/design/frontend/SimplifiedMagento/CustomTheme/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/luma', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/SimplifiedMagento/CustomTheme', __DIR__);
