e9579652eda57e0ee2cd130466a9babbd4bfa567
Alexandr Martynov <alex@magemore.com>
2021-07-15 13:07:21 +0300

    EavSetupFactory

--- web/app/code/SimplifiedMagento/Attribute/Setup/InstallData.php
+++ web/app/code/SimplifiedMagento/Attribute/Setup/InstallData.php
@@ -7,10 +7,11 @@ namespace SimplifiedMagento\Attribute\Setup;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
 
 class InstallData implements InstallDataInterface
 {
-    private $eavSetupFactory;
+    private EavSetupFactory $eavSetupFactory;
 
     public function __construct(EavSetupFactory $eavSetupFactory)
     {
@@ -24,7 +25,22 @@ class InstallData implements InstallDataInterface
     {
         $setup->startSetup();
 
-        
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'custom_eav',
+            [
+                'group'=>'Content',
+                'type' =>'text',
+                'global'=>\Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible'=>true,
+                'required' =>true,
+                'searchable'=>false,
+                'used_in_product_listing' =>true,
+                'label'=>'Custom EAV',
+                'input'=>'text'
+            ]
+        );
 
         $setup->endSetup();
     }
