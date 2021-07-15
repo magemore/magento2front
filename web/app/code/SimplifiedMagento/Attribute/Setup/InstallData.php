3906f701df0e1b7e6c169ccffbf4608ff2092523
Alexandr Martynov <alex@magemore.com>
2021-07-15 12:51:51 +0300

    fix

---
+++ web/app/code/SimplifiedMagento/Attribute/Setup/InstallData.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace SimplifiedMagento\Attribute\Setup;
+
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+
+    }
+}
