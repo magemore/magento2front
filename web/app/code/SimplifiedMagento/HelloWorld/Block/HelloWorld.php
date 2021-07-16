ec445495056da31bfec47b448f0978488247af0a
Alexandr Martynov <alex@magemore.com>
2021-07-16 15:36:50 +0300

    ViewModel

--- web/app/code/SimplifiedMagento/HelloWorld/Block/HelloWorld.php
+++ web/app/code/SimplifiedMagento/HelloWorld/Block/HelloWorld.php
@@ -9,20 +9,5 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class HelloWorld extends \Magento\Framework\View\Element\Template
 {
-    protected $product;
 
-    public function __construct(
-        ProductRepositoryInterface $productRepository,
-        Template\Context $context, array $data = [])
-    {
-        $this->product = $productRepository;
-        parent::__construct($context, $data);
-    }
-
-
-    public function getProductName()
-    {
-        $product = $this->product->get('24-MB01');
-        return $product->getName();
-    }
 }
--- web/app/code/SimplifiedMagento/HelloWorld/ViewModel/HelloView.php
+++ web/app/code/SimplifiedMagento/HelloWorld/ViewModel/HelloView.php
@@ -4,10 +4,29 @@
 namespace SimplifiedMagento\HelloWorld\ViewModel;
 
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class HelloView implements ArgumentInterface
 {
+
+    protected $product;
+
+    public function __construct(
+        ProductRepositoryInterface $productRepository
+    )
+    {
+        $this->product = $productRepository;
+    }
+
+
+    public function getProductName()
+    {
+        $product = $this->product->get('24-MB01');
+        return $product->getName();
+    }
+
+
     public function getHelloWorld()
     {
         return "This is from custom Block";
