b6109883dd8f53a7f3fe52198d4d915692eff3f7
Alexandr Martynov <alex@magemore.com>
2021-07-19 14:45:36 +0300

    fix

---
+++ web/app/code/Maxime/Jobs/Model/Source/Department.php
@@ -0,0 +1,40 @@
+<?php
+namespace Maxime\Jobs\Model\Source;
+
+class Department implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /**
+     * @var \Maxime\Jobs\Model\Department
+     */
+    protected $_department;
+
+    /**
+     * Constructor
+     *
+     * @param \Maxime\Jobs\Model\Department $department
+     */
+    public function __construct(\Maxime\Jobs\Model\Department $department)
+    {
+        $this->_department = $department;
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options[] = ['label' => '', 'value' => ''];
+        $departmentCollection = $this->_department->getCollection()
+            ->addFieldToSelect('entity_id')
+            ->addFieldToSelect('name');
+        foreach ($departmentCollection as $department) {
+            $options[] = [
+                'label' => $department->getName(),
+                'value' => $department->getId(),
+            ];
+        }
+        return $options;
+    }
+}
---
+++ web/app/code/Maxime/Jobs/Model/Source/Job/Status.php
@@ -0,0 +1,38 @@
+<?php
+namespace Maxime\Jobs\Model\Source\Job;
+
+class Status implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /**
+     * @var \Maxime\Jobs\Model\Job
+     */
+    protected $_job;
+
+    /**
+     * Constructor
+     *
+     * @param \Maxime\Jobs\Model\Job $job
+     */
+    public function __construct(\Maxime\Jobs\Model\Job $job)
+    {
+        $this->_job = $job;
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options[] = ['label' => '', 'value' => ''];
+        $availableOptions = $this->_job->getAvailableStatuses();
+        foreach ($availableOptions as $key => $value) {
+            $options[] = [
+                'label' => $value,
+                'value' => $key,
+            ];
+        }
+        return $options;
+    }
+}
