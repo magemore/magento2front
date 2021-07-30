7701b029cc0d4d0041c5e8bfb4ac708f70daaced
Alexandr Martynov <alex@magemore.com>
2021-07-30 22:17:03 +0300

    fix

--- web/app/code/Magemore/Register/Controller/Index/Index.php
+++ web/app/code/Magemore/Register/Controller/Index/Index.php
@@ -18,7 +18,7 @@ class Index extends \Magento\Framework\App\Action\Action
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
         )
     {
 
