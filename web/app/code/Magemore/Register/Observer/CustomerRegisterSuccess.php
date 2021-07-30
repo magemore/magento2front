8e09923b86e2e68a2ebc3b4ce35a2af2d5d8cc85
Alexandr Martynov <alex@magemore.com>
2021-07-30 21:31:33 +0300

    fix

--- web/app/code/Magemore/Register/Observer/CustomerRegisterSuccess.php
+++ web/app/code/Magemore/Register/Observer/CustomerRegisterSuccess.php
@@ -5,6 +5,7 @@ namespace Magemore\Register\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
+use Laminas\Mail;
 
 class CustomerRegisterSuccess implements ObserverInterface
 {
@@ -12,6 +13,7 @@ class CustomerRegisterSuccess implements ObserverInterface
     {
         $customer = $observer->getCustomer();
         $this->log($customer);
+        $this->sendMail($customer);
     }
 
     private function log($customer) {
@@ -22,5 +24,16 @@ class CustomerRegisterSuccess implements ObserverInterface
         $logger = new \Zend\Log\Logger();
         $logger->addWriter($writer);
         $logger->info($str);
+    }
+
+    private function sendMail($customer) {
+        $mail = new Mail\Message();
+        $mail->setBody('This is the text of the email.');
+        $mail->setFrom('Freeaqingme@example.org', "Sender's name");
+        $mail->addTo('Matthew@example.com', 'Name of recipient');
+        $mail->setSubject('TestSubject');
+
+        $transport = new Mail\Transport\Sendmail();
+        $transport->send($mail);
     }
 }
